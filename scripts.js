b9931dec44d7d34949509fc6b3363034bec2f472
Julien <julien.trotoux@gmail.com>
2015-02-11 23:50:36 +0100

    Ajout des versions

--- scripts.js
+++ scripts.js
@@ -2,8 +2,8 @@ var project = 'mydocteopro';
 
 // Static account (first step)
 var pros = new Array();
-pros.push(new Array('Sylvain Guillet','http://www.mydocteo.com/1'));
-pros.push(new Array('Julien Trotoux','http://www.mydocteo.com/2'));
+pros.push(new Array('Sylvain Guillet', 'http://www.mydocteo.com/1'));
+pros.push(new Array('Julien Trotoux', 'http://www.mydocteo.com/2'));
 
 
 // Get From LS (next step)
@@ -12,16 +12,16 @@ pros.push(new Array('Julien Trotoux','http://www.mydocteo.com/2'));
 // FI, myDocteo will share these informations while connecting
 //LocalStorage.save(project,pros);
 
-
-// if pro connection(s), just add shortcuts to the body
-if(pros.length) {
-	$("<ul/>").insertAfter($("body"));
-	for(var x=0;x<pros.length;x++) {
-		$("ul").append("<li><a href='"+pros[x][1]+"' target='_blank'>"+pros[x][0]+"</a></li>");
-	}
+// add link +
+$("body").append("<p><a href='http://www.mydocteo.com/membre/connexion.php' target='_blank'>+ Ajouter un compte</a></p>")
+if (pros.length) {
+    $("body").prepend("<ul/>");
+    for (var x = 0; x < pros.length; x++) {
+        $("ul").append("<li><a href='" + pros[x][1] + "' target='_blank'>" + pros[x][0] + "</a></li>");
+    }
 }
 // else inform there's no account and force to connect to myDocteo Pro
 else {
-	$("<p>Aucun espace pro n'est configuré<br/><a href='http://www.mydocteo.com'>Connectez-vous</a></p>").insertAfter($("body"));
+    $("body").prepend("<p>Aucun espace pro n'est configuré<br/><a href='http://www.mydocteo.com'>Connectez-vous</a></p>");
 }
 
